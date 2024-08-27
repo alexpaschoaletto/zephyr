a31adeb1a126f98922628f585114f3e4438a9c2a
Alexander pinheiro paschoaletto <1222703@isep.ipp.pt>
2024-08-27 13:19:03 +0100

    hotfixes and start of documentation

--- include/zephyr/server/internal/cbs_internal.h
+++ include/zephyr/server/internal/cbs_internal.h
@@ -18,6 +18,12 @@ typedef uint64_t cbs_cycle_t;
 typedef uint32_t cbs_cycle_t;
 #endif
 
+#ifdef CONFIG_TIMEOUT_64BIT
+#define CBS_TICKS_TO_CYC(t) k_ticks_to_cyc_floor32(t)
+#else
+#define CBS_TICKS_TO_CYC(t) k_ticks_to_cyc_floor64(t)
+#endif
+
 void cbs_thread_switched_in(struct k_thread *thread);
 void cbs_thread_switched_out(struct k_thread *thread);
 bool cbs_is_idle(struct k_thread *cbs_thread);
--- kernel/cbs.c
+++ kernel/cbs.c
@@ -8,5 +8,9 @@
 #include <zephyr/server/cbs.h>
 
 bool cbs_is_idle(struct k_thread *cbs_thread){
+    /*
+        This has to be here because z_is_thread_pending
+        is not visible to the subsys folder.
+    */
     return z_is_thread_pending(cbs_thread);
 }
